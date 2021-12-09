74fcbc24b0508bcb3c97ea86d4b201fe07fb3c0b
dimi3o <dimi3o@bk.ru>
2021-12-10 01:19:52 +0300

    animate

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.animation import Animation
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 from kivy.uix.gridlayout import GridLayout
-
 
 from os import listdir
 kv_path = './kv/'
@@ -14,13 +14,18 @@ for kv in listdir(kv_path):
 class AddButton(Button):
     pass
 
-
 class SubtractButton(Button):
     pass
 
+class SimpleButton(Button):
+    pass
 
 class Container(GridLayout):
     display = ObjectProperty()
+
+    def add_one(self):
+        value = int(self.display.text)
+        self.display.text = str(value+1)
 
     def add_one(self):
         value = int(self.display.text)
@@ -30,8 +35,24 @@ class Container(GridLayout):
         value = int(self.display.text)
         self.display.text = str(value-1)
 
+    def animate(self):
+        # create an animation object. This object could be stored
+        # and reused each call or reused across different widgets.
+        # += is a sequential step, while &= is in parallel
+        animation = Animation(pos=(100, 100), t='out_bounce')
+        animation += Animation(pos=(200, 100), t='out_bounce')
+        animation &= Animation(size=(500, 500))
+        animation += Animation(size=(100, 50))
+
+        self.btn.text = str(int(self.btn.text)+1)
+
+        # apply the animation on the button, passed in the "instance" argument
+        # Notice that default 'click' animation (changing the button
+        # color while the mouse is down) is unchanged.
+        animation.start(self.btn)
 
 class MainApp(App):
+
     def build(self):
         self.title = 'Awesome app!!!'
         return Container()
