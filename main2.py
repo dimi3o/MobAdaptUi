31da24cafb1ab17622efcf4d3814ba7fe32621d4
dimi3o <dimi3o@bk.ru>
2022-02-03 20:47:01 +0300

    right screen animation

--- main2.py
+++ main2.py
@@ -11,21 +11,22 @@ __version__ = '0.0.2.0'
 class MainApp(App):
     sm = ScreenManager()
     def build(self):
-        root1 = BoxLayout(orientation='vertical', padding=10)
-        root1.add_widget(Label(text='FIRST SCREEN'))
-        root1.add_widget(Button(text='To second screen',on_press=self.to_scr2_btn_click))
+        self.root1 = BoxLayout(orientation='vertical', padding=10)
+        self.root1.add_widget(Label(text='FIRST SCREEN'))
+        self.root1.add_widget(Button(text='To second screen',on_press=self.to_scr2_btn_click))
         scr1 = Screen(name='scr1')
-        scr1.add_widget(root1)
-        root2 = BoxLayout(orientation='vertical', padding=10)
-        root2.add_widget(Label(text='SECOND SCREEN'))
-        root2.add_widget(Button(text='To first screen', on_press=self.to_scr1_btn_click))
+        scr1.add_widget(self.root1)
+        self.root2 = BoxLayout(orientation='vertical', padding=10)
+        self.root2.add_widget(Label(text='SECOND SCREEN'))
+        self.root2.add_widget(Button(text='To first screen', on_press=self.to_scr1_btn_click))
         scr2 = Screen(name='scr2')
-        scr2.add_widget(root2)
+        scr2.add_widget(self.root2)
         self.sm.add_widget(scr1)
         self.sm.add_widget(scr2)
         return self.sm
 
     def to_scr1_btn_click(self, instance):
+        self.sm.transition.direction = 'right'
         self.sm.current = 'scr1'
 
     def to_scr2_btn_click(self, instance):
