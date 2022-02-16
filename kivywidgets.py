dc23f892fa70db03aef812c90a7defbc1c90b71b
dimi3o <dimi3o@bk.ru>
2022-02-17 00:01:02 +0300

    algorithm improvement

--- kivywidgets.py
+++ kivywidgets.py
@@ -13,14 +13,13 @@ from colors import allcolors, redclr, greenclr
 class Widgets():
     @staticmethod
     def get_random_widget():
-        widgets = ['Image', 'TextInput', 'Label', 'Button', 'CheckBox', 'Slider', 'Switch', 'Spinner', 'ProgressBar']
-        selected_name = random.choice(widgets)
-        if selected_name == 'Image': return Image(source='data/icons/bug1.png')
-        elif selected_name == 'TextInput': return TextInput(text='textinput')#(font_size = 20, size_hint_y = None, height = 50)
-        elif selected_name == 'Label': return Label(text='label', color=random.choice(allcolors))
-        elif selected_name == 'Button': return Button(text='button', background_color=random.choice(allcolors))#on_press=self.on_btn_click))
-        elif selected_name == 'CheckBox': return CheckBox(active = True)
-        elif selected_name == 'Slider': return Slider(min=1, max=10, value=1, step=1)
-        elif selected_name == 'Switch': return Switch(active = True)
-        elif selected_name == 'Spinner': return Spinner(text ="Spinner", values =("Python", "Java", "C++", "C", "C#", "PHP"), background_color =(0.784, 0.443, 0.216, 1))
-        elif selected_name == 'ProgressBar': return ProgressBar(max = 1000, value = 750)
+        widgets = [Image(source='data/icons/bug1.png'),
+                   TextInput(text='textinput'),#(font_size = 20, size_hint_y = None, height = 50)
+                   Label(text='label', color=random.choice(allcolors)),
+                   Button(text='button', background_color=random.choice(allcolors)), #on_press=self.on_btn_click))
+                   CheckBox(active=True),
+                   Slider(min=1, max=10, value=1, step=1),
+                   Switch(active=True),
+                   Spinner(text="Spinner", values=("Python", "Java", "C++", "C", "C#", "PHP"), background_color=(0.784,0.443,0.216,1)),
+                   ProgressBar(max=1000, value=750)]
+        return random.choice(widgets)
