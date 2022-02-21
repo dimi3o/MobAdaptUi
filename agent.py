0b27b72a9ed14ee55f6b0db5130378c48f327ff6
dimi3o <dimi3o@bk.ru>
2022-02-21 12:28:44 +0300

    agent anatomy

--- agent.py
+++ agent.py
@@ -1,7 +1,44 @@
+import random
 from numpy import np
 
+
+class Environment:
+    def __init__(self):
+        self.steps_left = 10
+
+    def get_observation(self):
+        return [0.0, 0.0, 0.0]
+
+    def get_actions(self):
+        return [0, 1]
+
+    def is_done(self):
+        return self.steps_left == 0
+
+    def action(self, action):
+        if self.is_done():
+            raise Exception("Game is over")
+        self.steps_left -= 1
+        return random.random()
+
 class Agent:
-    pass
+    def __init__(self):
+        self.total_reward = 0.0
+
+    def step(self, env):
+        current_obs = env.get_observation()
+        actions = env.get_actions()
+        reward = env.action(random.choice(actions))
+        self.total_reward += reward
+
+# if __name__ == "__main__":
+#     env = Environment()
+#     agent = Agent()
+#
+#     while not env.is_done():
+#         agent.step(env)
+#
+#     print("Total reward got: %.4f" % agent.total_reward)
 
 # Learning Intrinsic Symbolic Rewards in RL (2020)
 # https://arxiv.org/pdf/2010.03694.pdf
